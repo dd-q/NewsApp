9a69006b97bd45f20a5fa5e098fe87a88ccab70b
ddq <1210eatar@gmail.com>
2024-07-11 19:47:36 +0900

    OnBoardingPage 생성. (part 3)

---
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/Dimens.kt
@@ -0,0 +1,18 @@
+package com.loc.newsapp.presentation.onboarding
+
+import androidx.compose.ui.unit.dp
+
+
+/**
+ * https://www.youtube.com/watch?v=ZwmFEFRLrOA
+ * 4:00
+ *
+ * Padding Value 등 dimension 관련 수치들을 모아둘 Object.
+ */
+
+object Dimens {
+    val MediumPadding1 = 24.dp
+    val MediumPadding2 = 30.dp
+
+    val IndicatorSize = 6.dp
+}
